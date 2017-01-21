995d69ddbee22d0dc7cf82738f7686cbd3c869e0
Kantaris <kantarisinnovations.staff@gmail.com>
2017-01-21 17:11:38 -0600

    added a timeline feature and links into the top navbar along with routing and views for each.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Timelines
+router.get('/timelines', function(req, res){
+  res.render('timelines');
+});
+
+// Events
+router.get('/events', function(req, res){
+  res.render('events');
+});
+
+// People
+router.get('/people', function(req, res){
+  res.render('people');
+});
+
+// Places
+router.get('/places', function(req, res){
+  res.render('places');
+});
+
 module.exports = router;
